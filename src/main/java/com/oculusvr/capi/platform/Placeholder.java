fc80424a94c047d5939804ebdec8d67605f7fb03
Brad Davis <bradd@imdb.com>
2014-05-20 14:46:37 -0700

    Adding some stuff to force creation of a javadoc jar for maven central repo deploy

---
+++ src/main/java/com/oculusvr/capi/platform/Placeholder.java
@@ -0,0 +1,14 @@
+package com.oculusvr.capi.platform;
+
+/**
+ * This class only exists to thwart the Javadoc restrictions on creating
+ * javadoc for an empty package.
+ *  
+ * @author bradd
+ *
+ */
+public class Placeholder {
+  private Placeholder() {
+    
+  }
+}
---
+++ src/main/java/com/oculusvr/capi/platform/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains per-platform jars for using the Oculus C API on Java
+ * <p>
+ * Currently the supported platforms are 32 and 64 bit windows, and 64 
+ * bit java
+ */
+package com.oculusvr.capi.platform;
+
